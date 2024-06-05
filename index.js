5a131d3e2e04df43d5b825c6281e65b57079c61e
siam02 <siam.jaman02@gmail.com>
2024-06-05 16:42:08 +0600

    Get single parcel API added

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/parcels/:id', async (req, res) => {
+        app.get('/parcels-by-id/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await parcelCollection.findOne(query);
